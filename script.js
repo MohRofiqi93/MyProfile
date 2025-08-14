8d6b8fb4419e0668e1e75931394106699070b5db
MRofiqi <mohrofiqi1993@gmail.com>
2025-08-14 11:17:49 +0700

    Website Profile Moh Rofiqi

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scroll untuk navigasi menu
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    const href = this.getAttribute('href');
+    if (href.startsWith('#')) {
+      e.preventDefault();
+      const target = document.querySelector(href);
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  });
+});
